d0d4f6fb74806519b3a958cd7018094730abfb4d
Kennet Mathisen <kennetam02@gmail.com>
2026-06-15 17:39:28 +0200

    first iteration of omp prompt, want to add git integration and some others

--- modules/features/oh-my-posh.nix
+++ modules/features/oh-my-posh.nix
@@ -5,6 +5,32 @@
     packages.myOh-my-posh= inputs.wrapper-modules.wrappers.oh-my-posh.wrap {
       inherit pkgs;
       settings = {
+        blocks = [
+          {
+            type = "prompt";
+            alignment = "left";
+            segments = [
+              {
+                type = "path";
+                style = "plain";
+                properties.style = "full";
+                template = "{{ .Path }}";
+              }
+            ];
+          }
+          {
+            type = "prompt";
+            newline = true;
+            alignment = "left";
+            segments = [
+              {
+                type = "text";
+                style = "plain";
+                template = "  ";
+              }
+            ];
+          }
+        ];
       };
     };
   };
