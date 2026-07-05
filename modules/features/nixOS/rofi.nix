62e1c75ba368e2024998a16d85f0833baba309f7
Kennet Mathisen <kennetam02@gmail.com>
2026-07-05 16:11:57 +0200

    moved homeconfig to default

--- modules/features/nixOS/rofi.nix
+++
@@ -1,7 +0,0 @@
-{ self, inputs, ... }: {
-  perSystem = { pkgs, ... }: {
-    packages.myRofi = inputs.wrapper-modules.wrappers.rofi.wrap {
-      inherit pkgs;
-    };  
-  };
-}
--- modules/hosts/desktop/default.nix
+++ modules/hosts/desktop/default.nix
@@ -7,4 +7,15 @@
       self.nixosModules.myHomeManager
      ];
   };
+  flake.homeConfigurations.kennet = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+    modules = [
+      self.homeModules.kennetModule
+      {
+        home.username = "kennet";
+        home.homeDirectory = "/home/kennet";
+      }
+    ];
+  };
+
 }
