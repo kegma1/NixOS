be0413f1a4a203741bd492fb72c3305c271248a1
Kennet Mathisen <kennetam02@gmail.com>
2026-07-09 18:55:27 +0200

    some waybar styling, må gjøre den tynnere

--- modules/features/nixOS/waybar.nix
+++ modules/features/nixOS/waybar.nix
@@ -5,6 +5,55 @@
     stylix.targets.waybar.addCss = false;
     programs.waybar = {
       enable = true;    
+      style = lib.mkAfter ''
+          * {
+            all: unset;
+            font-family: "Iosevka Nerd Font Propo", monospace;
+          }
+
+          window#waybar {
+            background-color: alpha(@base00, 0.99);
+            color: @base05;
+            border-radius: 0 0 10px 10px;
+            opacity: 0.99;
+          }
+
+          #workspaces {
+            min-width: 100px;
+            padding: 2px 4px;
+            margin: 11px 0;
+            border-radius: 10px;
+          }
+
+          #workspaces button {
+            background-color: @base04;
+            padding: 0px 5px;
+            border-radius: 10px;
+            margin: 0 2px;
+            min-width: 4px;
+          }
+
+          #workspaces button.active {
+            background-color: @base0D;
+            min-width: 1.4rem;
+            transition: none;
+          }
+
+          #clock
+          #clock#date {
+            margin: 0 15px;
+            font-weight: 700;
+            font-size: 1.12rem;
+          }
+
+          .modules-left {
+            margin-left: 12px;
+          }
+
+          .module-right {
+            margin-right: 12px;
+          }
+        '';
       settings = {
         mainbar = {
           layer = "top";
