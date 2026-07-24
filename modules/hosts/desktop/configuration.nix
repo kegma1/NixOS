d1e68f42deb8f873d76339bd9571637e9073e7f3
Kennet Mathisen <kennetam02@gmail.com>
2026-07-24 16:25:45 +0200

    moved shii to home

--- modules/hosts/desktop/configuration.nix
+++ modules/hosts/desktop/configuration.nix
@@ -58,7 +58,6 @@
 
     programs.nix-index-database.comma.enable = true;
 
-
     xdg.portal = {
       enable = true;
       xdgOpenUsePortal = true;
@@ -79,7 +78,6 @@
     services.xserver.videoDrivers = ["nvidia"];
     hardware.nvidia.open = true; # see the note above
     hardware.cpu.amd.updateMicrocode = true;
-
 
     # Configure keymap in X11
     services.xserver.xkb = {
@@ -117,32 +115,7 @@
     };
     home-manager.users."kennet" = self.homeModules.kennetModule;
 
-
     environment.systemPackages = [
-      pkgs.alacritty
-      pkgs.spotify
-      pkgs.localsend
-      pkgs.libva-utils
-      pkgs.mission-center
-      pkgs.kdePackages.filelight
-      pkgs.cava
-      pkgs.qutebrowser
-      pkgs.solaar
-      pkgs.pavucontrol
-      pkgs.libnotify
-      pkgs.playerctl
-      (pkgs.writeShellScriptBin "fixisaac"
-        ''
-          pid=$(pgrep -n -f 'isaac-ng.exe')
-
-          sudo ${lib.getExe pkgs.gdb} -q -n -batch -p "$pid" \
-          -ex 'set pagination off' \
-          -ex 'python inf=gdb.selected_inferior(); inf.write_memory(0x00a9e941, bytes.fromhex("6a 01 ff 15 d8 82 b1 00 e9 50 01 00 00"))' \
-          -ex 'python inf=gdb.selected_inferior(); inf.write_memory(0x00a9e9c6, bytes.fromhex("e9 76 ff ff ff"))' \
-          -ex 'x/5bx 0x00a9e9c6' \
-          -ex 'x/13bx 0x00a9e941' \
-          -ex 'detach'
-        '')
     ];
 
     system.stateVersion = "26.05";
--- modules/hosts/desktop/home.nix
+++ modules/hosts/desktop/home.nix
@@ -3,7 +3,11 @@
   inputs,
   ...
 }: {
-  flake.homeModules.kennetModule = {pkgs, config, ...}: {
+  flake.homeModules.kennetModule = {
+    pkgs,
+    config,
+    ...
+  }: {
     imports = with self.homeModules; [
       desktop
       cli
@@ -15,5 +19,33 @@
 
     qt.enable = true;
     home.stateVersion = "24.11";
+
+    home.packages = with pkgs; [
+      lavat
+      alacritty
+      spotify
+      localsend
+      libva-utils
+      mission-center
+      kdePackages.filelight
+      cava
+      qutebrowser
+      solaar
+      pavucontrol
+      libnotify
+      playerctl
+      (pkgs.writeShellScriptBin "fixisaac"
+        ''
+          pid=$(pgrep -n -f 'isaac-ng.exe')
+
+          sudo ${lib.getExe pkgs.gdb} -q -n -batch -p "$pid" \
+          -ex 'set pagination off' \
+          -ex 'python inf=gdb.selected_inferior(); inf.write_memory(0x00a9e941, bytes.fromhex("6a 01 ff 15 d8 82 b1 00 e9 50 01 00 00"))' \
+          -ex 'python inf=gdb.selected_inferior(); inf.write_memory(0x00a9e9c6, bytes.fromhex("e9 76 ff ff ff"))' \
+          -ex 'x/5bx 0x00a9e9c6' \
+          -ex 'x/13bx 0x00a9e941' \
+          -ex 'detach'
+        '')
+    ];
   };
 }
